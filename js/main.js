af92f71594fca85e999470dd1f6448bffab6251d
bcudina <bozidar.cudina@gmail.com>
2019-02-04 01:18:17 +0100

    04022019 html, css i java pocetak

--- js/main.js
+++ js/main.js
@@ -1,12 +1,44 @@
-function myFunction7() {
-document.getElementById("demo7").innerHTML = "7";
-   }
-      
-        
-function myFunction8() {
-document.getElementById("demo8").innerHTML = "8";
-   }
+var oneBtn = document.getElementById("br_one");
+var twoBtn = document.getElementById("br_two");
+var treeBtn = document.getElementById("br_tree");
+var fourBtn = document.getElementById("br_four");
+var fiveBtn = document.getElementById("br_five");
+var sixBtn = document.getElementById("br_six");
+var sevenBtn = document.getElementById("br_seven");
+var eightBtn = document.getElementById("br_eight");
+var nineBtn = document.getElementById("br_nine");
+var zeroBtn = document.getElementById("br_zero");
 
-function Function_mnozenje () {
-document.getElementById("mnozenje").innerHTML = Math.pow(8,7);
-    }
+var decimalBtn = document.getElementById("calc_decimal");
+var clearlBtn = document.getElementById("calc_clear");
+var bekspaceBtn = document.getElementById("calc_bekspace");
+
+var displayValElement = document.getElementById("ekran");
+
+var calcNumBtns = document.getElementsByClassName("tipke_brojke");
+var calcOperBtns = document.getElementsByClassName("tipke_opr");
+
+var displayVal = "0";
+var pendingVal;
+var evalStringArray = [];
+
+var updateDisplayVal = (clickObj) => {
+    var btnText = clickObj.target.innerText;
+    
+    if(displayVal === "0")
+        displayVal = " ";
+    displayVal += btnText;
+    displayValElement.innerText = displayVal;
+}
+
+for (let i=0; i < calcNumBtns.lengts; i++) {
+    calcNumBtns[i].addEventListener("click", updateDisplayVal, false);
+}
+
+for (let i=0; i < calcOperBtns.lengts; i++) {
+    calcOperBtns[i].addEventListener("click", performOperation, false);
+}
+
+
+
+
