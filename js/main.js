2286c6f5a0e82b4081dd924789390b46319d3349
bcudina <bozidar.cudina@gmail.com>
2019-02-13 00:21:25 +0100

    13022019_kraj

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
  
 
-/*prvi dio do znaka = sluzi u javascript dokumentuza daljnje operacije,
+/*prvi dio do znaka = sluzi u javascript dokumentu za daljnje operacije,
 a drugi dio povezuje js sa html dokumentom*/
 
 var oneBtn = document.getElementById("br_one");     /*zove id broja 1 */
@@ -24,15 +24,20 @@ var divideBtn = document.getElementById('calc-dicide');
 var multiplyBtn = document.getElementById('calc-multiply');
 var plusBtn = document.getElementById('calc-plus');
 /*ekran */
-var displayValElement = document.getElementById("ekran"); 
+var display_ekran = document.getElementById('ekran'); 
 /*zove class tipki i to brojke*/
 var calcTipkeBrojke = document.getElementsByClassName("tipke_brojke");  
 /*zove class tipki i to operatere*/
-var calcOperatorTipke = document.getElementsByClassName("tipke_opr");
+var calcOperatorTipke = document.getElementsByClassName("tipke_operater");
     
-var displayVal = "0";   /*pocetni display uvijek pokazuje 0 */
+var pocetni_ekran = "0";   /*pocetni display uvijek pokazuje 0 */
 var pendingVal;         /* 5 + 3, samo ce 3 biti prikazano, zadnja tipka*/
 var evalStringArray = []; /*pretvara u stringove i automatski zbraja*/
+
+/* ------------------------------------------------------------- */
+/* ------------------------------------------------------------- */
+/* ------------------------------------------------------------- */
+/* ------------------------------------------------------------- */
     
 /*definiramo "numberClick", pocetni disply uvijek je 0*/
 var numberClick = (num) => {
@@ -41,64 +46,120 @@ var numberClick = (num) => {
     displayVal += num;
     displayValElement.innerText = displayVal; /*ovo je funkcija UPDATE */
 };
+var numberClick = (clickObj) => {
+    if(pocetni_ekran === '0')
+        pocetni_ekran = '';
+    pocetni_ekran += clickObj;
+    display_ekran.innerHTML = pocetni_ekran;
+    };
 
 
     /*kada kliknemo na broj */
 
 /*kada kliknemo na operater */
-var updateDisplayVal = (clickObj) => {
+var update_ekran = (clickObj) => {
     let btnText = clickObj.target.innerText;
     numberClick(btnText);
-};
+    };
 
 
 /* pocetak vezan uz tipke operacija */
 /* ------------------------------------------------------------- */
-var tipke_operacija = (clickObj) => {
-    var operation = clickObj.target.innerHTML;
-    switch (operation) {
+/* ------------------------------------------------------------- */
+/* ------------------------------------------------------------- */
+/* ------------------------------------------------------------- */
+/* ------------------------------------------------------------- */
+
+
+
+
+var performOperation = (clickObj) => {           /*dodajemo varijabli*/
+    var operation = clickObj.target.innerText;   /*koji objekt smo kliknuli*/
+    
+    switch (operation) {         /*u slucaju da je operator npr "+" uradi ovo*/
         case "+":
-            pendingVal = Number.parseFloat(displayVal);
-            displayVal = "";
-            displayValElement.innerHTML = displayVal;
+            pendingVal = Number.parseFloat(pocetni_ekran);
+            pocetni_ekran = '';
+            display_ekran.innerHTML = pocetni_ekran; /*update gornji red*/
             evalStringArray.push(pendingVal);
-            evalStringArray.push('+');
+            evalStringArray.push("+");
             break;
             
+        case "-":
+            pendingVal = Number.parseFloat(pocetni_ekran);
+            pocetni_ekran = '';
+            display_ekran.innerHTML = pocetni_ekran; /*update gornji red*/
+            evalStringArray.push(pendingVal);
+            evalStringArray.push("-");
+            break;
+            
+        case "*":
+            pendingVal = Number.parseFloat(pocetni_ekran);
+            pocetni_ekran = '';
+            display_ekran.innerHTML = pocetni_ekran; /*update gornji red*/
+            evalStringArray.push(pendingVal);
+            evalStringArray.push("*");
+            break;
+            
+        case "/":
+            pendingVal = Number.parseFloat(pocetni_ekran);
+            pocetni_ekran = '';
+            display_ekran.innerHTML = pocetni_ekran; /*update gornji red*/
+            evalStringArray.push(pendingVal);
+            evalStringArray.push("/");
+            break;
+            
+        case "=":
+            evalStringArray.push(pocetni_ekran);
+            var evaluation = eval(evalStringArray.join(""));
+            pocetni_ekran = evaluation
+            display_ekran.innerHTML = evaluation;
+            pocetni_ekran = evaluation;
+            pendingVal = pocetni_ekran;
+            evalStringArray = [];
+            pocetni_ekran = evaluation + '';
+                    
         default:
             break;
     }
-}
+    
+};
 
-/* ------------------------------------------------------------- */
 
 /*kada kliknemo na neki broj u class "tipke_brojke", Listener
-zove funkciju "updateDisplayVal"*/
+zove funkciju "update_ekran"*/
 /*pitanje je mora li var biti definiran prije ili moze poslije*/
-for (let i=0; i < calcTipkeBrojke.length; i++) {
-    calcTipkeBrojke[i].addEventListener("click", updateDisplayVal, false);
+for (var i=0; i < calcTipkeBrojke.length; i++) {
+    calcTipkeBrojke[i].addEventListener("click", update_ekran, false);
+    };
+for (var i=0; i < calcOperatorTipke.length; i++) {
+    calcOperatorTipke[i].addEventListener("click",performOperation, false);
     }
-for (let i=0; i < calcOperatorTipke.length; i++) {
-    calcOperatorTipke[i].addEventListener("click",updateDisplayVal, false);
-    }
+
+
+/* ------------------------------------------------------------- */
+/* ------------------------------------------------------------- */
+/* ------------------------------------------------------------- */
+/* ------------------------------------------------------------- */
+
 
 clearlBtn.onclick = () => {
-                displayVal = '0';
-                pendingVal = undefined;
-                evalStringArray = [];
-                displayValElement.innerHTML = displayVal;
-    }
+    pocetni_ekran = '0';
+    pendingVal = undefined;
+    evalStringArray = [];
+    display_ekran.innerHTML = pocetni_ekran;
+    };
 
 bekspaceBtn.onclick = () => {
-    let lengthOfDisplayVal = displayVal.length;
-    displayVal = displayVal.slice(0, lengthOfDisplayVal -1);
-    if (displayVal === "")
-        displayVal = "0";
-    displayValElement.innerText = displayVal;   
-    }
+    let lengthOfpocetni_ekran = pocetni_ekran.length;
+    pocetni_ekran = pocetni_ekran.slice(0, lengthOfpocetni_ekran -1);    /*brise zadnji simbol*/
+    if (pocetni_ekran === "")                    /*ako vise nema simbola*/
+        pocetni_ekran = "0";                     /*prikazi nulu*/
+    display_ekran.innerText = pocetni_ekran;   
+    };
 
 decimalBtn.onclick = ()=> {                     /*var decimalBtn na click*/
-    if(!displayVal.includes ("."))              /*ako je na displaju vec tocka*/
-        displayVal += ".";                      /*vise od jedne*/
-    displayValElement.innerText = displayVal;   /*update*/
-}
+    if(!pocetni_ekran.includes ("."))           /*ako je na displaju vec tocka*/
+        pocetni_ekran += ".";                   /*vise od jedne*/
+    display_ekran.innerText = pocetni_ekran;    /*update*/
+    };
